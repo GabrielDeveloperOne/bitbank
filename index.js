e9d948e4d9947de74514012abb8102d40ca81fc0
Gabriel <gabriel.developer@outlook.com>
2020-05-18 20:42:58 -0300

    entendo os metodos

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ class Cliente {
     cpf;
 }
 
-class ContaCorrente {
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor) {
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 
@@ -21,13 +27,13 @@ cliente2.cpf = 11111111111;
 const contaCorrenteGabriel = new ContaCorrente();
 contaCorrenteGabriel.saldo = 0;
 contaCorrenteGabriel.agencia = 1001;
+
 console.log(contaCorrenteGabriel.saldo);
 contaCorrenteGabriel.saldo = 100;
 console.log(contaCorrenteGabriel.saldo);
-let valorSacado = 200;
-if(contaCorrenteGabriel.saldo >= valorSacado){
-    contaCorrenteGabriel -= valorSacado;
-}
+contaCorrenteGabriel.sacar(500);
+console.log(contaCorrenteGabriel.saldo);
 
+console.log(contaCorrenteGabriel);
 console.log(cliente1);
 console.log(cliente2);
