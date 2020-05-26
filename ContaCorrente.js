b3fa88dc8d16e6d4ba072eeeb0f2f5d9e4b156f7
Gabriel <gabriel.developer@outlook.com>
2020-05-25 21:22:03 -0300

    atributos staticos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,11 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
+    static numeroDeContas = 0;
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
     }
 
     set cliente(novoValor){
--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const cliente1 = new Cliente("Gabriel", 123456789);
 const cliente2 = new Cliente("Olegario", 123498765);
 console.log(cliente2.cpf);
 
-const contaCorrenteGabriel = new ContaCorrente(1001, cliente1);
+const contaCorrenteGabriel = new ContaCorrente(cliente1, 1001);
 contaCorrenteGabriel.depositar(500);
+console.log(contaCorrenteGabriel);
 
 const conta2 = new ContaCorrente(cliente1, 102);
 
 let valor = 200;
 contaCorrenteGabriel.transferir(valor, conta2)
+
+console.log(ContaCorrente.numeroDeContas);
 
 
 
